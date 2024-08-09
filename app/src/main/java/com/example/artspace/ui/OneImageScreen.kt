845e19fd18df45f86da3d163cdd45f9f263b4b9e
crismz <crismuz1999@gmail.com>
2024-08-09 14:01:15 -0300

    Added HorinzontalPager to scroll between pages

--- app/src/main/java/com/example/artspace/ui/OneImageScreen.kt
+++ app/src/main/java/com/example/artspace/ui/OneImageScreen.kt
@@ -1,12 +1,15 @@
 package com.example.artspace.ui
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
@@ -15,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,7 +29,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.artspace.R
 import data.Art
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun OneImageScreen(
     imageSelected: Int,
@@ -35,71 +41,86 @@ fun OneImageScreen(
     var index by remember { mutableIntStateOf(imageSelected) }
     val size: Int = arts.size
 
-    Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        OutlinedButton(
-            onClick = onCancelButtonClicked,
-            colors = ButtonDefaults.outlinedButtonColors(
-                containerColor = Color.Gray,
-                contentColor = Color.Black
-            ),
-            border = BorderStroke(1.dp, Color.DarkGray),
-            modifier = Modifier
-                .padding(top = 10.dp)
-                .weight(0.5f)
-        ) {
-            Text(stringResource(R.string.back))
-        }
-        Image(
-            painter = painterResource(arts[index].image),
-            contentDescription = arts[index].name,
-            modifier = Modifier.weight(4f)
-        )
-        Column (
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.weight(1f)
-        ){
-            Text(
-                text = arts[index].name,
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = arts[index].year,
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = "Author: ${arts[index].author}",
-                fontWeight = FontWeight.Bold
-            )
-        }
-        Row (
-            modifier = Modifier.weight(0.5f)
-        ){
-            Spacer(
-                modifier = Modifier.weight(0.5f)
-            )
-            Button(
-                onClick = { index = if (index == 0) (size-1) else (index-1) % size },
-                modifier = Modifier.weight(2f)
-            ) {
-                Text("Previous")
-            }
-            Spacer(
-                modifier = Modifier.weight(0.5f)
-            )
-            Button(
-                onClick = { index = (index+1) % size },
-                modifier = Modifier.weight(2f)
-            ) {
-                Text("Next")
-            }
-            Spacer(
-                modifier = Modifier.weight(0.5f)
-            )
-        }
+    val pageCount = size * 800
 
+    val pagerState = rememberPagerState(
+        pageCount = { pageCount },
+        initialPage = (pageCount/2) + index
+    )
+    val scope = rememberCoroutineScope()
+
+    HorizontalPager(state = pagerState) { page ->
+        index = page % size
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            OutlinedButton(
+                onClick = onCancelButtonClicked,
+                colors = ButtonDefaults.outlinedButtonColors(
+                    containerColor = Color.Gray,
+                    contentColor = Color.Black
+                ),
+                border = BorderStroke(1.dp, Color.DarkGray),
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .weight(0.5f)
+            ) {
+                Text(stringResource(R.string.back))
+            }
+            Image(
+                painter = painterResource(arts[index].image),
+                contentDescription = arts[index].name,
+                modifier = Modifier.weight(4f)
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = arts[index].name,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = arts[index].year,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Author: ${arts[index].author}",
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Row(
+                modifier = Modifier.weight(0.5f)
+            ) {
+                Spacer(
+                    modifier = Modifier.weight(0.5f)
+                )
+                Button(
+                    onClick = { scope.launch {
+                        pagerState.animateScrollToPage(pagerState.currentPage - 1)
+                    }},
+                    modifier = Modifier.weight(2f)
+                ) {
+                    Text("Previous")
+                }
+                Spacer(
+                    modifier = Modifier.weight(0.5f)
+                )
+                Button(
+                    onClick = { scope.launch {
+                        pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                    }},
+                    modifier = Modifier.weight(2f)
+                ) {
+                    Text("Next")
+                }
+                Spacer(
+                    modifier = Modifier.weight(0.5f)
+                )
+            }
+
+        }
     }
 
 }
